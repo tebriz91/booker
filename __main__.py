feba33f9c798884a92822625446349c80fa1da2a
tebriz91 <tebriz.tagiyev@gmail.com>
2024-01-31 22:48:09 +0300

    first commit

---
+++ __main__.py
@@ -0,0 +1,120 @@
+import logging
+import config
+import handlers
+from db import close_db
+
+from telegram.ext import (
+    ApplicationBuilder,
+    CallbackQueryHandler,
+    CommandHandler,
+    MessageHandler,
+    filters,
+)
+
+def main():
+    application = ApplicationBuilder().token(config.TELEGRAM_BOT_TOKEN).build()
+
+    initialize_database()
+    create_superadmin
+    register_handlers()
+
+    COMMAND_HANDLERS = {
+        "start": handlers.start,
+        "help": handlers.help_,
+        "allbooks": handlers.all_books,
+        "already": handlers.already,
+        "now": handlers.now,
+        "vote": handlers.vote,
+        "cancel": handlers.cancel,
+        "voteresults": handlers.vote_results,
+    }
+
+    CALLBACK_QUERY_HANDLERS = {
+        rf"^{config.ALL_BOOKS_CALLBACK_PATTERN}(\d+)$": handlers.all_books_button,
+        rf"^{config.VOTE_BOOKS_CALLBACK_PATTERN}(\d+)$": handlers.vote_button,
+    }
+
+
+
+
+
+
+
+
+
+
+
+import logging
+import config
+import handlers
+from db import close_db
+
+from telegram.ext import (
+    ApplicationBuilder,
+    CallbackQueryHandler,
+    CommandHandler,
+    MessageHandler,
+    filters,
+)
+
+
+
+
+from botanim_bot import config, handlers
+from botanim_bot.db import close_db
+
+COMMAND_HANDLERS = {
+    "start": handlers.start,
+    "help": handlers.help_,
+    "allbooks": handlers.all_books,
+    "already": handlers.already,
+    "now": handlers.now,
+    "vote": handlers.vote,
+    "cancel": handlers.cancel,
+    "voteresults": handlers.vote_results,
+}
+
+CALLBACK_QUERY_HANDLERS = {
+    rf"^{config.ALL_BOOKS_CALLBACK_PATTERN}(\d+)$": handlers.all_books_button,
+    rf"^{config.VOTE_BOOKS_CALLBACK_PATTERN}(\d+)$": handlers.vote_button,
+}
+
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+
+if not config.TELEGRAM_BOT_TOKEN or not config.TELEGRAM_BOTANIM_CHANNEL_ID:
+    raise ValueError(
+        "TELEGRAM_BOT_TOKEN and TELEGRAM_BOTANIM_CHANNEL_ID env variables "
+        "wasn't implemented in .env (both should be initialized)."
+    )
+
+
+def main():
+    application = ApplicationBuilder().token(config.TELEGRAM_BOT_TOKEN).build()
+
+    for command_name, command_handler in COMMAND_HANDLERS.items():
+        application.add_handler(CommandHandler(command_name, command_handler))
+
+    for pattern, handler in CALLBACK_QUERY_HANDLERS.items():
+        application.add_handler(CallbackQueryHandler(handler, pattern=pattern))
+
+    application.add_handler(
+        MessageHandler(filters.TEXT & (~filters.COMMAND), handlers.vote_process)
+    )
+
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception:
+        import traceback
+
+        logger.warning(traceback.format_exc())
+    finally:
+        close_db()
